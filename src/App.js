9be699d4d908cbd61525a6010184920b6c9ea558
kayalvizhi <madhukayal88@gmail.com>
2026-05-15 18:02:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
+import React, { useState } from "react";
+import ColourSelector from "./ColourSelector";
+import Selection from "./Selection";
 
+function App() {
+  const [selectedColor, setSelectedColor] = useState("");
+
+  const colors = ["red", "green", "blue"];
+
+  return (
+    <div>
+      <h1>Passing Handler Prop</h1>
+
+      {colors.map((color) => (
+        <ColourSelector
+          key={color}
+          color={color}
+          setSelectedColor={setSelectedColor}
+        />
+      ))}
+
+      <Selection selectedColor={selectedColor} />
+    </div>
+  );
+}
+
+export default App;
