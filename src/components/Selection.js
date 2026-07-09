1bf3154204a344eec78da3eaf5941ed93d9ecf15
kayalvizhi <madhukayal88@gmail.com>
2026-07-09 10:34:05 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Selection({ selectedColor }) {
+  const [color, setColor] = useState("");
+
+  const handleClick = () => {
+    setColor(selectedColor);
+  };
+
   return (
     <div style={{ marginTop: "20px" }}>
       <div
         className="fix-box"
-        style={{ backgroundColor: selectedColor }}
+        style={{ backgroundColor: color }}
+        onClick={handleClick}
       ></div>
 
       <div
         className="fix-box"
-        style={{ backgroundColor: selectedColor }}
+        style={{ backgroundColor: color }}
+        onClick={handleClick}
       ></div>
 
       <div
         className="fix-box"
-        style={{ backgroundColor: selectedColor }}
+        style={{ backgroundColor: color }}
+        onClick={handleClick}
       ></div>
     </div>
   );
