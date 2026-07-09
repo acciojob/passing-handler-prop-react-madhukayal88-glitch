3559e83a3d4eaced53d1ec2de84ed21729590c0a
madhukayal88-glitch <madhukayal88@gmail.com>
2026-07-09 10:45:15 +0530

    Fix Selection component

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -3,28 +3,14 @@ import React, { useState } from "react";
 function Selection({ selectedColor }) {
   const [color, setColor] = useState("");
 
-  const handleClick = () => {
-    setColor(selectedColor);
-  };
-
   return (
-    <div style={{ marginTop: "20px" }}>
+    <div
+      style={{ marginTop: "20px" }}
+      onClick={() => setColor(selectedColor)}
+    >
       <div
         className="fix-box"
         style={{ backgroundColor: color }}
-        onClick={handleClick}
-      ></div>
-
-      <div
-        className="fix-box"
-        style={{ backgroundColor: color }}
-        onClick={handleClick}
-      ></div>
-
-      <div
-        className="fix-box"
-        style={{ backgroundColor: color }}
-        onClick={handleClick}
       ></div>
     </div>
   );
